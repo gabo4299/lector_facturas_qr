9a5133fa5091be1b37e2e117d26311f11febba4f
Gabriel Bellido <gabrielcuellar99@hotmail.com>
2025-10-29 17:44:56 -0400

    dev o prod change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import fs from 'fs'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  server:{
-    https: {
-      key: fs.readFileSync('../localhost+4-key.pem'),
-      cert: fs.readFileSync('../localhost+4.pem'),
-    },
-     host: true, // permite conexiones externas (0.0.0.0)
-    port: 5173, // o el puerto que prefieras
-  }
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  server: mode === 'development'
+    ? {
+        https: {
+          key: fs.readFileSync('../localhost+4-key.pem'),
+          cert: fs.readFileSync('../localhost+4.pem'),
+        },
+        host: true,
+        port: 5173,
+      }
+    : {}, // En producción no usar certificados
+}))
